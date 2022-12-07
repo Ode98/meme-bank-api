07123a39f30b927ff3919f7a332f46d12bfdcab3
Ode98 <mrotto100@gmail.com>
2022-12-07 20:59:33 +0200

    Initial backend setup

---
+++ config/index.js
@@ -0,0 +1,14 @@
+const Cloud = require("@google-cloud/storage");
+const path = require("path");
+
+const serviceKey = path.join(__dirname, "./key.json");
+
+console.log(serviceKey);
+
+const { Storage } = Cloud;
+const storage = new Storage({
+  keyFilename: serviceKey,
+  projectId: "poised-backbone-370813",
+});
+
+module.exports = storage;
